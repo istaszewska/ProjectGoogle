eafac82ab4330efb359d96cc56ca1cc98ade444d
istaszewska <76110460+istaszewska@users.noreply.github.com>
2022-10-19 22:18:28 +0200

    Changes to HomePage

--- test-logic/src/test/java/com/google/homepage/tests/SearchTest.java
+++ test-logic/src/test/java/com/google/homepage/tests/SearchTest.java
@@ -1,19 +1,27 @@
 package com.google.homepage.tests;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.Keys;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchTest extends AbstractTest {
 
     String searchPhrase = "apple";
+    String randomText = RandomStringUtils.random(30, true, true);
 
     @Test
     public void validateSearchResults() {
         List<String> listOfResults = homePage.acceptCookies()
-                .typeIntoUserNameField(searchPhrase)
+                .typeInSearchField(searchPhrase)
                 .clickSearchButton()
                 .getSearchResults();
 
@@ -21,8 +29,35 @@ public class SearchTest extends AbstractTest {
     }
 
     @Test
-    public void validateRemoteKeyboard(){
+    public void validateInvalidSearch() {
+        homePage.acceptCookies()
+                .typeInSearchField(randomText)
+                .getElementAndPressKey(homePage.getSearchInputBox(), Keys.ENTER);
 
+        String expectedErrorMessage = "Podana fraza - " + randomText + " - nie została odnaleziona.";
+        String actualErrorMessage = homePage.getErrorMessage();
+
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage);
     }
 
+    @ParameterizedTest
+    @MethodSource("calculationData")
+    public void validateSearchCalculations(String calculation, String expectedResult){
+        homePage.acceptCookies()
+                .typeInSearchField(calculation)
+                .getElementAndPressKey(homePage.getSearchInputBox(), Keys.ENTER);
+
+        String calculationResult = homePage.getCalculationResult().getText();
+
+        Assertions.assertEquals(expectedResult, calculationResult);
+    }
+
+    private static Stream<Arguments> calculationData() {
+        return Stream.of(
+                Arguments.of("2 + 2" , "4"),
+                Arguments.of("-1 * 1", "-1"),
+                Arguments.of("-1 / -1", "1"),
+                Arguments.of("0,1 - 0,1", "0")
+        );
+    }
 }
